8d2f6c5a0625689d6560633bedee35db4b14b072
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2023-10-14 15:24:23 +0330

    <feat> : member model added.

---
+++ tascrum/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from Auth.models import User
+
+
+
+
+class Member(models.Model):
+
+    occupations_choice = (
+    ('Student','Student'),
+    ('Employee','Employee'),
+    ('Project Manager','Project Manager'),
+    ('Other','Other'),
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,related_name='users',null=False)\
+    
+    occupations = models.CharField(max_length=255,null=False,choices=occupations_choice)
+    bio = models.TextField()
+    profimage = models.ImageField(upload_to='Member/Profile',null=False,default='default_profile.png')
+    birthdate = models.DateField()
+
+
