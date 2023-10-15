2b804f0d0d87cb60c2f5d5fa15c61d05be242237
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2023-10-15 20:36:08 +0330

    <fix> : workspace for each member in their profile is available.

--- tascrum/serializers.py
+++ tascrum/serializers.py
@@ -2,13 +2,23 @@ from rest_framework import serializers
 from .models import Member,Workspace
 from Auth.serializers import UserProfileSerializer
 
+
+
+class WorkspaceMemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workspace
+        fields = ['name', 'type', 'description']
+
 class MemberSerializer(serializers.ModelSerializer):
     user = UserProfileSerializer()
+    workspaces = serializers.SerializerMethodField()
     class Meta:
         model = Member
-        fields = ['id', 'occupations', 'bio' ,'profimage', 'birthdate' ,  "user"]
-
-
+        fields = ['id', 'occupations', 'bio', 'profimage', 'birthdate', 'user', 'workspaces']
+        
+    def get_workspaces(self, obj):
+        workspaces = obj.wmembers.all()
+        return WorkspaceSerializer(workspaces, many=True).data
 
 
 
