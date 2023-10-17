942d4c3c722cba94a11bed28dbb8f39d206d089f
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2023-10-17 11:47:12 +0330

    <fix> : changing username_field to email.

--- Auth/models.py
+++ Auth/models.py
@@ -3,19 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    # occupation_choice = (
-    #     ('employee','employee'),
-    #     ('student','student'),
-    #     ('project manager','project manager'),
-    #     ('other','other'),
-    # )
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    # occupation = models.CharField(choices=occupation_choice,max_length=20,default='other')
     email = models.EmailField(unique=True)
     username = models.CharField(default='user',null=True,max_length=20,unique=True)
-    USERNAME_FIELD = "username"
-    # REQUIRED_FIELDS = ['username','password']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username','password']
 
     groups = models.ManyToManyField(
         'auth.Group',
