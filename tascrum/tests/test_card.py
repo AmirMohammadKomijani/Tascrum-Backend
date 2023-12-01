a09ef9533a022e9a73c54d8c5955688266ff1ad0
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2023-12-01 19:51:48 +0330

    <fix> : fixing create card. still not working.

--- tascrum/tests/test_card.py
+++ tascrum/tests/test_card.py
@@ -130,60 +130,59 @@ class CreateCardViewTest(APITestCase, SimpleTestCase):
         self.client.credentials(HTTP_AUTHORIZATION=f'JWT {token}')
     
 
-    # def test_Create_Card_POST(self):
-    #     self.authenticate()
-    #     user1 = User.objects.create_user(first_name='saba', last_name='razi', email='razi1.saba@gmail.com',
-    #                                      username="test username", password='thisissaba')
-    #     self.members = Member.objects.create(
-    #         user=user1,
-    #         occupations='Employee',
-    #         bio='Another test bio',
-    #         birthdate='1990-05-15'
-    #     )
-    #     self.workspace = Workspace.objects.create(name='workspace test2', type='small business',
-    #                                               description='description test', backgroundImage='')
-    #     self.board = Board.objects.create(
-    #         title='board test',
-    #         backgroundImage="",
-    #         workspace=self.workspace
-    #     )
+    def test_Create_Card_POST(self):
+        self.authenticate()
+        user1 = User.objects.create_user(first_name='saba', last_name='razi', email='razi1.saba@gmail.com',
+                                         username="test username", password='thisissaba')
+        self.members = Member.objects.create(
+            user=user1,
+            occupations='Employee',
+            bio='Another test bio',
+            birthdate='1990-05-15'
+        )
+        self.workspace = Workspace.objects.create(name='workspace test2', type='small business',
+                                                  description='description test', backgroundImage='')
+        self.board = Board.objects.create(
+            title='board test',
+            backgroundImage="",
+            workspace=self.workspace
+        )
 
-    #     # Ensure Member is associated with the Board as an owner
-    #     board_role = MemberBoardRole.objects.filter(member=self.members, board=self.board).first()
-    #     if not board_role or board_role.role != "owner":
-    #         # Create MemberBoardRole if it doesn't exist or the role is not "owner"
-    #         board_role = MemberBoardRole.objects.create(member=self.members, board=self.board, role='owner')
+        # Ensure Member is associated with the Board as an owner
+        # board_role = MemberBoardRole.objects.filter(member=self.members, board=self.board).first()
+        # if not board_role or board_role.role != "owner":
+        #     # Create MemberBoardRole if it doesn't exist or the role is not "owner"
+        #     board_role = MemberBoardRole.objects.create(member=self.members, board=self.board, role='owner')
 
-    #     self.list = List.objects.create(title='List test', board=self.board)
+        self.list = List.objects.create(title='List test', board=self.board)
 
-    #     create_card_data = {
-    #         "id": 28,
-    #         "title": "card 4 list 6",
-    #         "list": self.list.id,
-    #         "startdate": "2023-11-29T12:53:05.642000Z",
-    #         "duedate": "2023-11-27T11:13:51.230361Z",
-    #         "reminder": "At time of due date",
-    #         "storypoint": 0,
-    #         "setestimate": 0,
-    #         "description": "test",
-    #         "status": "pending"
-    #     }
+        create_card_data = {
+            "title": "card 4 list 6",
+            "list": self.list.id,
+            "startdate": "2023-11-29T12:53:05.642000Z",
+            "duedate": "2023-11-27T11:13:51.230361Z",
+            "reminder": "At time of due date",
+            "storypoint": 0,
+            "setestimate": 0,
+            "description": "test",
+            "status": "pending"
+        }
 
-    #     # Assign the board_role to the card
-    #     create_card_data["board_role"] = board_role.id
+        # Assign the board_role to the card
+        # create_card_data["board_role"] = board_role.id
 
-    #     response = self.client.post(self.create_card_url, create_card_data, format='json')
-    #     print(response.content)
+        response = self.client.post(self.create_card_url, create_card_data, format='json')
+        print(response.content)
 
-    #     # Check if the response status code is 201 (Created)
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Check if the response status code is 201 (Created)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    #     # Optionally, you can check the response data for additional details
-    #     self.assertEqual(response.data['title'], 'card 4 list 6')
+        # Optionally, you can check the response data for additional details
+        self.assertEqual(response.data['title'], 'card 4 list 6')
 
-    #     # Optionally, you can check if the new card is actually created in the database
-    #     new_card = Card.objects.get(title='card 4 list 6', list=self.list)
-    #     self.assertIsNotNone(new_card)
+        # Optionally, you can check if the new card is actually created in the database
+        new_card = Card.objects.get(title='card 4 list 6', list=self.list)
+        self.assertIsNotNone(new_card)
 
     
     # def test_card_count(self):
